a7e958b112dcd055399a656c048d6615748d0c39
TugbaAydinnn <66336394+TugbaAydinnn@users.noreply.github.com>
2021-08-20 14:27:45 +0300

    Initial commit

---
+++ LoginSreen/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginSreen.Models;
+
+
+
+namespace LoginSreen.Controllers
+{
+    public class LoginController : Controller
+    {
+        DbMusteriEntities db = new DbMusteriEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult MusteriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult MusteriEkle(MusteriBilgileri p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+            db.MusteriBilgileri.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult SifreKontrol(MusteriBilgileri p)
+        {
+
+            var bilgi = db.MusteriBilgileri.FirstOrDefault(x => x.KullaniciAdi == p.KullaniciAdi && x.Sifre==p.Sifre);
+
+            if (bilgi!=null){
+
+                return RedirectToAction("MusteriEkle",bilgi);
+            }
+
+            return RedirectToAction("Index");
+        }
+      
+    }
+}
